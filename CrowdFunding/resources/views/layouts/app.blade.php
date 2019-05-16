fbff53046b57c75ec4142720f4c161e81118a82e
鈴木将彦 <ternbicycles2013@gmail.com>
2019-05-16 23:54:52 +0900

    ログイン画面モーダル実装

--- CrowdFunding/resources/views/layouts/app.blade.php
+++ CrowdFunding/resources/views/layouts/app.blade.php
@@ -46,7 +46,77 @@
 												<a class="nav-link" href="#login">{{ __('ログイン') }}</a>
 												</li>
 													<!-- ログイン画面モーダル -->
-													
+														<div class="modal fade" id="LoginModal" tabindex="-1" role="dialog" aria-labelledby="LoginModalCenterTitle" aria-hidden="true">
+															<div class="modal-dialog modal-dialog-centered" role="document">
+																<div class="modal-content">
+																	<div class="modal-header">
+																		<h5 class="modal-title" id="LoginModalCenterTitle">ログイン画面</h5>
+																		<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+																			<span aria-hidden="true">&times;</span>
+																		</button>
+																	</div>
+																	<div class="modal-body">
+																		<form method="POST" action="{{ route('login') }}">
+																			@csrf
+
+																			<div class="form-group row">
+																				<label for="email" class="col-md-4 col-form-label text-md-right">{{ __('メールアドレス') }}</label>
+
+																				<div class="col-md-6">
+																					<input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+																					@error('email')
+																						<span class="invalid-feedback" role="alert">
+																							<strong>{{ $message }}</strong>
+																						</span>
+																					@enderror
+																				</div>
+																			</div>
+
+																			<div class="form-group row">
+																				<label for="password" class="col-md-4 col-form-label text-md-right">{{ __('パスワード') }}</label>
+
+																				<div class="col-md-6">
+																					<input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+																					@error('password')
+																						<span class="invalid-feedback" role="alert">
+																							<strong>{{ $message }}</strong>
+																						</span>
+																					@enderror
+																				</div>
+																			</div>
+
+																			<div class="form-group row">
+																				<div class="col-md-6 offset-md-4">
+																					<div class="form-check">
+																						<input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+																						<label class="form-check-label" for="remember">
+																							{{ __('ログイン状態を保存') }}
+																						</label>
+																					</div>
+																				</div>
+																			</div>
+
+																			<div class="form-group row mb-0">
+																				<div class="col-md-8 offset-md-4">
+																					<button type="submit" class="btn btn-primary">
+																						{{ __('ログイン') }}
+																					</button>
+
+																					@if (Route::has('password.request'))
+																						<a class="btn btn-link" href="{{ route('password.request') }}">
+																							{{ __('パスワードを忘れた方はこちら') }}
+																						</a>
+																					@endif
+																				</div>
+																			</div>
+																		</form>
+																	</div>
+																</div>
+															</div>
+														</div>
 														@if (Route::has('register'))
 																<li class="nav-item">
 																		<a class="nav-link" href="{{ route('register') }}">{{ __('新規登録') }}</a>
