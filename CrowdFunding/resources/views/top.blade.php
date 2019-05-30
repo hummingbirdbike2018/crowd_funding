736959a76024232749040c2b5ddc8dd363554569
鈴木将彦 <ternbicycles2013@gmail.com>
2019-05-31 01:22:36 +0900

    TOPバナーに検索ボックス設置(レスポンシブ未対応)

--- CrowdFunding/resources/views/top.blade.php
+++ CrowdFunding/resources/views/top.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')
 
-	<div class="content">
-		<img src="img/header_1.png" class="img-fluid topbanner" alt="Responsive image">
+	<div class="content top_bannner">
+		<img src="img/header_1.png" class="img-fluid">
 	</div>
-	<div class="input-group mx-3 mb-1">
-		<input type="text" class="form-control" placeholder="プロジェクトを検索" aria-label="Recipient's username" aria-describedby="button-addon2">
-		<div class="col-md-9 top-search">
-			<button class="btn btn-primary" type="button" id="button-addon2">検索</button>
+	<div class="input-group position-absolute top_search">
+		<input type="text" class="form-control rounded-0" placeholder="プロジェクトを検索" aria-label="Recipient's username" aria-describedby="button-addon2">
+		<div class="col-md-9">
+			<button class="btn btn-primary rounded-0" type="button" id="button-addon2">検索</button>
 		</div>
 	</div>
 @endsection
