85db894c3dca59b5a769d9b885f35e2a48c4d911
渡辺敬之 <takayuki.wata7be@hotmail.co.jp>
2019-06-16 19:40:35 +0900

    メール認証機能追加

--- CrowdFunding/database/migrations/2019_05_26_104028_create_user_table.php
+++ CrowdFunding/database/migrations/2019_05_26_104028_create_user_table.php
@@ -6,39 +6,39 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateUserTable extends Migration
 {
-		/**
-		 * Run the migrations.
-		 *
-		 * @return void
-		 */
-		public function up()
-		{
-				Schema::create('users', function (Blueprint $table) {
-						$table->increments('id')->unsigned;
-						$table->integer('user_id');
-						$table->string('display');
-						$table->string('name');
-						$table->string('furigana');
-						$table->integer('tel');
-						$table->integer('post_code');
-						$table->string('address');
-						$table->string('building');
-						$table->string('email')->unique();
-						$table->string('password');
-						$table->integer('disable');
-						$table->integer('dis_reason');
-						$table->rememberToken();
-						$table->timestamps();
-				});
-		}
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function (Blueprint $table) {
+			$table->increments('id')->unsigned;
+			$table->integer('user_id');
+			$table->string('display')->nullable();
+			$table->string('name')->nullable();
+			$table->string('name_kana')->nullable();
+			$table->integer('tel')->nullable();
+			$table->integer('post_code')->nullable();
+			$table->string('address')->nullable();
+			$table->string('building')->nullable();
+			$table->string('email')->unique();
+			$table->string('password');
+			$table->integer('disable')->nullable();
+			$table->integer('dis_reason')->nullable();
+			$table->rememberToken();
+			$table->timestamps();
+		});
+	}
 
-		/**
-		 * Reverse the migrations.
-		 *
-		 * @return void
-		 */
-		public function down()
-		{
-				Schema::dropIfExists('users');
-		}
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('users');
+	}
 }
--- CrowdFunding/routes/web.php
+++ CrowdFunding/routes/web.php
@@ -11,27 +11,31 @@
 |
 */
 //Route::group(['middleware' => 'auth'], function() {
-	//ホーム画面
-	Route::get('/', 'HomeController@index')->name('top');
-	//利用規約
-	Route::get('/terms', function () { return view('terms'); });
-	//会員情報変更
-	Route::get('user/{id}/edit', 'UserController@index');
-	Route::post('user/{id}/edit/store', 'UserController@store');
-	//マイページ
-	Route::get('/user/{id}/top', function () { return view('user/top'); });
-	//退会
-	Route::get('/user/{id}/disable', function () { return view('user/disable'); });
-	//問い合わせフォーム
-	Route::get('/contact', 'HomeController@index')->name('contact');
-	Route::post('/contact', 'HomeController@store')->name('contact');
-	//掲載に関するご相談
-	Route::get('draft', 'DraftController@index');
-	Route::post('draft/store', 'DraftController@store');
-	//プロジェクトページ
-	Route::get('projects/{pj_id}', 'ProjectController@index');
-	// Route::post('draft/confirm', 'DraftController@confirm')->name('confirm');
-	// Route::post('draft/complete', 'DraftController@confirm')->name('complete');
-	//});
-	//認証機能
-	Auth::routes();
+//ホーム画面
+Route::get('/', 'HomeController@index')->name('top');
+//利用規約
+Route::get('/terms', function () { return view('terms'); });
+//会員情報変更
+Route::get('user/{id}/edit', 'UserController@index');
+Route::post('user/{id}/edit/store', 'UserController@store');
+//マイページ
+Route::get('/user/{id}/top', function () { return view('user/top'); });
+//退会
+Route::get('/user/{id}/disable', function () { return view('user/disable'); });
+//問い合わせフォーム
+Route::get('/contact', 'HomeController@index')->name('contact');
+Route::post('/contact', 'HomeController@store')->name('contact');
+//掲載に関するご相談
+Route::get('draft', 'DraftController@index');
+Route::post('draft/store', 'DraftController@store');
+//プロジェクトページ
+Route::get('projects/{pj_id}', 'ProjectController@index');
+// Route::post('draft/confirm', 'DraftController@confirm')->name('confirm');
+// Route::post('draft/complete', 'DraftController@confirm')->name('complete');
+//});
+//認証機能
+Auth::routes();
+
+
+// 本人確認URL
+Route::get('register/verify/{token}', 'Auth\RegisterController@showForm');
