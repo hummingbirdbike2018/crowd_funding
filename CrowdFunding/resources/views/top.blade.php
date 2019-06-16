0d3a7d0079b42218c8de87d0516333c5dc504fd7
渡辺敬之 <takayuki.wata7be@hotmail.co.jp>
2019-06-16 18:53:03 +0900

    フォルダ構成修正

--- CrowdFunding/resources/views/top.blade.php
+++ CrowdFunding/resources/views/top.blade.php
@@ -1,17 +1,11 @@
-@extends('layout')
-
+@extends('layouts.layout')
 @section('content')
-
-	<div class="content top_bannner">
-		<img src="img/header_1.png" class="img-fluid">
-	</div>
+	<div class="top_bannner"></div>
 	<div class="input-group position-absolute top_search">
-		<input type="text" class="form-control rounded-0" placeholder="プロジェクトを検索" aria-label="Recipient's username" aria-describedby="button-addon2">
+		<!-- <input type="text" class="form-control rounded-0" placeholder="プロジェクトを検索" aria-label="Recipient's username" aria-describedby="button-addon2">
 		<div class="col-md-9">
 			<button class="btn btn-primary rounded-0" type="button" id="button-addon2">検索</button>
-		</div>
+		</div> -->
 	</div>
-
 @endsection
-
 @include('footer')
