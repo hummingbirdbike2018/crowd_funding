9f0f31ec205db26357c861af8d883a5da9e4b041
渡辺敬之 <takayuki.wata7be@hotmail.co.jp>
2019-08-04 20:33:49 +0900

    Seederを修正

--- CrowdFunding/database/seeds/RewardsTableSeeder.php
+++ CrowdFunding/database/seeds/RewardsTableSeeder.php
@@ -5,14 +5,15 @@ use Carbon\Carbon;
 
 class RewardsTableSeeder extends Seeder
 {
-		/**
-		 * Run the database seeds.
-		 *
-		 * @return void
-		 */
-		public function run()
-		{
-			DB::table('rewards')->insert([
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		DB::table('rewards')->insert([
+			[
 				'id' => 1,
 				'pj_id' => 1,
 				'rw_title' => 'リターン1',
@@ -23,6 +24,31 @@ class RewardsTableSeeder extends Seeder
 				'rw_season' => '2019年8月',
 				'created_at' => Carbon::now(),
 				'updated_at' => Carbon::now(),
-			]);
-		}
+			],
+			[
+				'id' => 2,
+				'pj_id' => 1,
+				'rw_title' => 'リターン2',
+				'rw_body' => '＜リターンの詳細について＞',
+				'rw_image' => 'reward_image.jpg',
+				'rw_quantity' => '500',
+				'rw_price' => '20000',
+				'rw_season' => '2019年8月',
+				'created_at' => Carbon::now(),
+				'updated_at' => Carbon::now(),
+			],
+			[
+				'id' => 3,
+				'pj_id' => 2,
+				'rw_title' => 'リターン1',
+				'rw_body' => '＜リターンの詳細について＞',
+				'rw_image' => 'reward_image.jpg',
+				'rw_quantity' => '200',
+				'rw_price' => '15000',
+				'rw_season' => '2019年8月',
+				'created_at' => Carbon::now(),
+				'updated_at' => Carbon::now(),
+			]
+		]);
+	}
 }
--- CrowdFunding/database/seeds/SupportTableSeeder.php
+++ CrowdFunding/database/seeds/SupportTableSeeder.php
@@ -5,20 +5,38 @@ use Carbon\Carbon;
 
 class SupportTableSeeder extends Seeder
 {
-		/**
-		 * Run the database seeds.
-		 *
-		 * @return void
-		 */
-		public function run()
-		{
-			DB::table('supports')->insert([
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		DB::table('supports')->insert([
+			[
 				'user_id' => 1,
 				'reward_id' => 1,
 				'pj_id' => 1,
 				'comment' => '商品の到着楽しみです！',
 				'created_at' => Carbon::now(),
 				'updated_at' => Carbon::now(),
-			]);
-		}
+			],
+			[
+				'user_id' => 1,
+				'reward_id' => 1,
+				'pj_id' => 2,
+				'comment' => '商品の到着楽しみです！',
+				'created_at' => Carbon::now(),
+				'updated_at' => Carbon::now(),
+			],
+			[
+				'user_id' => 1,
+				'reward_id' => 2,
+				'pj_id' => 1,
+				'comment' => '商品の到着楽しみです！',
+				'created_at' => Carbon::now(),
+				'updated_at' => Carbon::now(),
+			]
+		]);
+	}
 }
