2ec54f3ee283a9d628c940103e7ae5ced07c3bc5
渡辺敬之 <takayuki.wata7be@hotmail.co.jp>
2019-08-12 19:50:10 +0900

    ルーティング設定修正

--- CrowdFunding/routes/web.php
+++ CrowdFunding/routes/web.php
@@ -10,39 +10,40 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-//Route::group(['middleware' => 'auth'], function() {
+
 //ホーム画面
 Route::get('/', 'HomeController@index')->name('top');
 //利用規約
 Route::get('/terms', function () { return view('terms'); });
-//会員情報変更
-Route::get('user/{id}/edit', 'UserController@edit');
-Route::post('user/{id}/edit/store', 'UserController@store');
-//マイページ
-Route::get('/user/{id}/top', function () { return view('user/top'); });
-//退会
-Route::get('/user/{id}/disable', function () { return view('user/disable'); });
-//掲載に関するご相談
-Route::get('draft', 'DraftController@index');
-Route::post('draft/store', 'DraftController@store');
 //プロジェクトページ
 Route::get('/projects/{id}', 'ProjectController@index');
 //支援選択ページ
 Route::get('/projects/{id}/supports/select', 'SupportController@index')->name('select');
-//個別支援ページ
-Route::get('/projects/{id}/supports/{reward_id}', 'SupportController@showSelectedReward')->name('selected');
-Route::post('/projects/{id}/supports/{reward_id}', 'SupportController@storeSelectedReward');
-//支援内容確認ページ
-Route::post('/projects/{id}/supports/{reward_id}/confirm', 'SupportController@confirmSelectedReward')->name('confirm');
-//支援完了ページ
-Route::post('/projects/{id}/supports/{reward_id}/complete', 'SupportController@completeSelectedReward')->name('complete');
-
+//掲載に関するご相談
+Route::get('draft', 'DraftController@index');
+Route::post('draft/store', 'DraftController@store');
 // Route::post('draft/confirm', 'DraftController@confirm')->name('confirm');
 // Route::post('draft/complete', 'DraftController@confirm')->name('complete');
-//});
+
+Route::group(['middleware' => ['auth']], function() {
+	//会員情報変更
+	Route::get('user/{id}/edit', 'UserController@edit');
+	Route::post('user/{id}/edit/store', 'UserController@store');
+	//マイページ
+	Route::get('/user/{id}/top', function () { return view('user/top'); });
+	//退会
+	Route::get('/user/{id}/disable', function () { return view('user/disable'); });
+	//個別支援ページ
+	Route::get('/projects/{id}/supports/{reward_id}', 'SupportController@showSelectedReward')->name('selected');
+	Route::post('/projects/{id}/supports/{reward_id}', 'SupportController@storeSelectedReward');
+	//支援内容確認ページ
+	Route::post('/projects/{id}/supports/{reward_id}/confirm', 'SupportController@confirmSelectedReward')->name('confirm');
+	//支援完了ページ
+	Route::post('/projects/{id}/supports/{reward_id}/complete', 'SupportController@completeSelectedReward')->name('complete');
+});
+
 //認証機能
 Auth::routes();
-
 // お問い合わせフォーム
 Route::get('contact', 'ContactController@index')->name('contact');
 Route::post('contact/confirm', 'ContactController@confirm')->name('confirm');
