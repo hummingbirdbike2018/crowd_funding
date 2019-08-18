d10fe01dffb9739281f7f3f380bd87f76f047bf6
鈴木将彦 <ternbicycles2013@gmail.com>
2019-08-18 20:02:14 +0900

    ドロップダウンメニューのデザイン変更

--- CrowdFunding/resources/views/top.blade.php
+++ CrowdFunding/resources/views/top.blade.php
@@ -17,28 +17,32 @@
 			@endguest
 		</div>
 	</div>
-	<div class="block_col">
-		<h2 class="block_title">PROJECTS<span class="sub_title">プロジェクト一覧</span></h2>
-	</div>
 	<div class="container">
-				<div class="cbox">
+		<div class="block_col">
+			<h2 class="block_title">PROJECTS<span class="sub_title">プロジェクト一覧</span></h2>
+		</div>
+			<div class="cbox">
 			@foreach($projects as $key => $project)
 				<div class="cbox-inner d-flex">
 					<div class="shadow-sm p-3 mb-5 bg-white rounded">
-						<img class="pj_img" src="../storage/product_img/project_{{$project->id}}/{{$project->product_img_1}}">
-						<h5 class="card-title mt-3">{!! nl2br(e( $project->pj_title )) !!}</h5>
-						<div class="card_planner_info"><i class="fa fa-user"></i>{{$planner_list[$key]}}</div>
-						<div class="card_progress">
-							<div class="card_total_amount">￥ {{number_format($total_amounts[$key])}}</div>
-							<div class="progress">
-								<div class="progress-bar progress-bar bg-warning"
-									role="progressbar" aria-valuenow="{{$percent_completes[$key]}}" aria-valuemin="0"
-									aria-valuemax="100" style="width:{{$percent_completes[$key]}}%">{{$percent_completes[$key]}}%
+						<div class="project-successs-bar">
+							<img class="pj_img" src="../storage/product_img/project_{{$project->id}}/{{$project->product_img_1}}">
+						</div>
+						<div class="project_detail">
+							<h5 class="card-title mt-3">{!! nl2br(e( $project->pj_title )) !!}</h5>
+							<div class="card_planner_info"><i class="fa fa-user"></i>{{$planner_list[$key]}}</div>
+							<div class="card_progress">
+								<div class="card_total_amount">￥ {{number_format($total_amounts[$key])}}</div>
+								<div class="progress">
+									<div class="progress-bar progress-bar bg-warning"
+										role="progressbar" aria-valuenow="{{$percent_completes[$key]}}" aria-valuemin="0"
+										aria-valuemax="100" style="width:{{$percent_completes[$key]}}%">{{$percent_completes[$key]}}%
+									</div>
 								</div>
-							</div>
-							<div class="card_progress_info">
-								<i class="fa fa-users"></i> {{$supporters[$key]}} 人が支援　
-								<i class="fa fa-clock"></i> あと {{$period[$key]}} 日
+								<div class="card_progress_info">
+									<i class="fa fa-users"></i> {{$supporters[$key]}} 人が支援　
+									<i class="fa fa-clock"></i> あと {{$period[$key]}} 日
+								</div>
 							</div>
 						</div>
 					</div>
@@ -46,6 +50,73 @@
 				</div>
 			@endforeach
 	</div>
+	<!-- 新着プロジェクト一覧 -->
+	<!-- <div class="block_col mt-5">
+		<h2 class="block_title">NEW PROJECT<span class="sub_title">新着プロジェクト</span></h2>
+	</div>
+	<div class="cbox">
+
+		<div class="cbox-inner d-flex">
+			<div class="shadow-sm p-3 mb-5 bg-white rounded">
+				<div class="project-successs-bar">
+					<img class="pj_img" src="../storage/product_img/project_{{$project->id}}/{{$project->product_img_1}}">
+				</div>
+				<div class="project_detail">
+					<h5 class="card-title mt-3">{!! nl2br(e( $project->pj_title )) !!}</h5>
+					<div class="card_planner_info"><i class="fa fa-user"></i>{{$planner_list[$key]}}</div>
+					<div class="card_progress">
+						<div class="card_total_amount">￥ {{number_format($total_amounts[$key])}}</div>
+						<div class="progress">
+							<div class="progress-bar progress-bar bg-warning"
+								role="progressbar" aria-valuenow="{{$percent_completes[$key]}}" aria-valuemin="0"
+								aria-valuemax="100" style="width:{{$percent_completes[$key]}}%">{{$percent_completes[$key]}}%
+							</div>
+						</div>
+						<div class="card_progress_info">
+							<i class="fa fa-users"></i> {{$supporters[$key]}} 人が支援
+							@if($period[$key] === 0)
+							<i class="fa fa-clock"></i> 終了
+							@else
+							<i class="fa fa-clock"></i> 販売中
+							@endif
+						</div>
+					</div>
+				</div>
+			</div>
+			<a href="./projects/{{$project->id}}"></a>
+		</div>
+	</div>
+	<!-- 達成したプロジェクト一覧 -->
+	<!-- <div class="block_col mt-5">
+		<h2 class="block_title">SUCCEED<span class="sub_title">達成したプロジェクト</span></h2>
+	</div>
+	<div class="cbox">
+		<div class="cbox-inner d-flex">
+			<div class="shadow-sm p-3 mb-5 bg-white rounded">
+				<div class="project-successs-bar">
+					<img class="pj_img" src="../storage/product_img/project_{{$project->id}}/{{$project->product_img_1}}">
+				</div>
+				<div class="project_detail">
+					<h5 class="card-title mt-3">{!! nl2br(e( $project->pj_title )) !!}</h5>
+					<div class="card_planner_info"><i class="fa fa-user"></i>{{$planner_list[$key]}}</div>
+					<div class="card_progress">
+						<div class="card_total_amount">￥ {{number_format($total_amounts[$key])}}</div>
+						<div class="progress">
+							<div class="progress-bar progress-bar bg-warning"
+								role="progressbar" aria-valuenow="{{$percent_completes[$key]}}" aria-valuemin="0"
+								aria-valuemax="100" style="width:{{$percent_completes[$key]}}%">{{$percent_completes[$key]}}%
+							</div>
+						</div>
+						<div class="card_progress_info">
+							<i class="fa fa-users"></i> {{$supporters[$key]}} 人が支援　
+							<i class="fa fa-clock"></i> あと {{$period[$key]}} 日
+						</div>
+					</div>
+				</div>
+			</div>
+			<a href="./projects/{{$project->id}}"></a>
+		</div>
+	</div> -->
 </div>
 @endsection
 @include('footer')
